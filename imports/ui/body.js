d23860464510d7beebd36056661965fb636e1ab8
sunyamare <sunyamare@gmail.com>
2020-04-05 18:44:02 +0200

    data from collection

---
+++ imports/ui/body.js
@@ -0,0 +1,10 @@
+import { Template } from "meteor/templating";
+import { Tasks } from "../api/tasks.js";
+
+import "./body.html";
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
