29f0e3ebf3c2158d2e61606a94020263ff812eb8
sunyamare <sunyamare@gmail.com>
2020-04-07 22:21:31 +0200

    methods and publish and subscribe

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,4 +1,6 @@
+import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
 import { Tasks } from "../api/tasks.js";
 
 // This file, because it is called the same as body.html (and in the same folder), is automatically used by meteor to add functionality...
@@ -7,11 +9,29 @@ import "./body.html";
 // Importing task.js also imports task.html to body.html?!
 import "./task.js";
 
+// initialize the state in a ReactiveDict
+Template.body.onCreated(function () {
+  Template.instance().state = new ReactiveDict();
+  Meteor.subscribe("tasks");
+});
+
 // creating a helper ("tasks") we use in body.html
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find(
+        { isChecked: { $ne: true } }, // $ne selects the documents where the value of the field is not equal to the specified value.
+        { sort: { createdAt: -1 } }
+      );
+    }
+    // Otherwise, return all of the tasks
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ isChecked: { $ne: true } }).count();
   },
 });
 
@@ -22,11 +42,17 @@ Template.body.events({
     // Get value from form element
     const text = event.target.text.value;
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    // Tasks.insert({
+    //   text,
+    //   createdAt: new Date(), // current time
+    //   owner: Meteor.userId(), // add current user's ID and username
+    //   username: Meteor.user().username,
+    // });
+    Meteor.call("tasks.insert", text);
     // Clear form
     event.target.text.value = "";
+  },
+  "change .hide-completed input"(event, instance) {
+    instance.state.set("hideCompleted", event.target.checked);
   },
 });
